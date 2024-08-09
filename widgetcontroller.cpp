7f3c04a28322503208c1411606ba87c82fcdac44
unknown <jejuvita01@gmail.com>
2024-08-09 15:32:48 +0900

    edited WidgetController

--- widgetcontroller.cpp
+++ widgetcontroller.cpp
@@ -1,17 +1,34 @@
 #include "loginwidget.h"
+#include "ui_loginwidget.h"
+#include "ui_mainwidget.h"
+#include "mainwidget.h"
 #include "widgetcontroller.h"
-
 #include <QWidget>
 #include <QObject>
 #include <QMainWindow>
+#include <map>
+#include <string>
+#include "Person.h"
+
+using namespace std;
 
 WidgetController::WidgetController(QWidget* parent)
     : QMainWindow(parent)
 {
-    lgnwdg = new loginwidget(this);
-    setCentralWidget(lgnwdg);
+    mainwdg = new MainWidget(this);
+    lgnwdg = new LoginWidget(this);
+    setCentralWidget(mainwdg);
+    lgnwdg->hide();
+    Ui::MainWidget *mainUi = mainwdg->getUi();
+    connect(mainUi->loginBtn, &QPushButton::clicked, this, [=] () {
+        mainwdg->hide();
+        lgnwdg->show();
+    });
 }
 
-WidgetController::~WidgetController() {
-
+void WidgetController::set_data(map<string, pair<string, Person*>> data)
+{
+    this->data = data;
 }
+
+WidgetController::~WidgetController() {}
--- widgetcontroller.h
+++ widgetcontroller.h
@@ -1,19 +1,48 @@
 #ifndef WIDGETCONTROLLER_H
 #define WIDGETCONTROLLER_H
-
 #include <QMainWindow>
 #include <QWidget>
+#include <map>
+#include <string>
+#include "loginwidget.h"
+#include "mainwidget.h"
+
+using namespace std;
+
 class QObject;
-class loginwidget;
+class LoginWidget;
+class Person;
+class MainWidget;
+
+namespace Ui {
+class MainWidget;
+class LoginWidget;
+}
 
 class WidgetController : public QMainWindow
 {
     Q_OBJECT
 private:
-    loginwidget* lgnwdg;
+    MainWidget *mainwdg;
+    LoginWidget* lgnwdg;
+    map<string, pair<string, Person*>> data; // id: <pw,person>
 public:
     WidgetController(QWidget* parent = nullptr);
+    void set_data(map<string, pair<string, Person*>> data);
     ~WidgetController();
 };
-
 #endif // WIDGETCONTROLLER_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
