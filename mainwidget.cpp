208b1cf516114da4c208fba0dc464c2cee3fd678
박성민 <sm136599@gmail.com>
2024-08-10 17:13:19 +0900

    feat: 메인 화면 버튼 기능 구현

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -1,14 +1,17 @@
 #include "mainwidget.h"
 #include "ui_mainwidget.h"
-
-class QPushButton;
-class QLabel;
+#include <QPushButton>
+#include <QLabel>
 
 MainWidget::MainWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
+    connect(ui->endBtn, &QPushButton::clicked, this, [=]() { emit quit(); });
+    connect(ui->loginBtn, &QPushButton::clicked, this, [=]() { emit switchToLoginScreen(); });
+    connect(ui->signUpBtn, &QPushButton::clicked, this, [=]() { emit switchToSignUpScreen(); });
+    connect(ui->showBtn, &QPushButton::clicked, this, [=]() { emit switchToShowProductScreen(); });
 }
 
 Ui::MainWidget* MainWidget::getUi()
