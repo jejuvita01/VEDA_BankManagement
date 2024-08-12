7fb4d497406f5b2abb6d3e48b27fee53e194daa7
박성민 <sm136599@gmail.com>
2024-08-12 23:14:38 +0900

    feat: 매니저 화면 동작 구현

--- managerwidget.cpp
+++ managerwidget.cpp
@@ -1,14 +1,129 @@
 #include "managerwidget.h"
 #include "ui_managerwidget.h"
+#include "person.h"
+#include "user.h"
+#include "manager.h"
+#include "account.h"
+#include "deposit.h"
+#include "saving.h"
+#include "utility.h"
+
+#include <vector>
+#include <QTableWidget>
+#include <QTableWidgetItem>
 
 ManagerWidget::ManagerWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::ManagerWidget)
 {
     ui->setupUi(this);
+    connect(ui->userSearchlineEdit, &QLineEdit::textChanged, this, [=](const QString& QId) {
+        refreshUserSearchTable(QId.toStdString());
+    });
+
+    connect(ui->logoutBtn, &QPushButton::clicked, this, [=]() {
+        qDebug() << "로그 아웃";
+        setPerson(nullptr);
+        emit switchTomainScreen();
+    });
+
+    connect(ui->withdrawBtn, &QPushButton::clicked, this, [=]() {
+        qDebug() << "매니저 탈퇴";
+        setPerson(nullptr);
+        emit withdrawManager(this->id);
+    });
 }
 
 ManagerWidget::~ManagerWidget()
 {
     delete ui;
+}
+
+void ManagerWidget::setData(map<string, pair<string, Person*>> d) {
+    this->data = d;
+}
+
+void ManagerWidget::setPerson(Person* person) {
+    this->person = person;
+}
+
+void ManagerWidget::refreshUserSearchTable(string searchId) {
+    ui->userSearchTableWidget->reset();
+
+    int row = 0;
+    QTableWidgetItem* item;
+    for (const auto& datum : data) {
+        if (dynamic_cast<Manager*>(datum.second.second)) continue;
+        string userId = datum.first;
+        if (userId.find(searchId) == string::npos) continue;
+
+        User* user = dynamic_cast<User*>(datum.second.second);
+        vector<Account*> userAccounts = user->get_accounts();
+
+        for (const auto& account : userAccounts) {
+            item = new QTableWidgetItem(QString::fromStdString(userId));
+            ui->userSearchTableWidget->setItem(row, 0, item);
+
+            if (dynamic_cast<Deposit*>(account)){
+                item = new QTableWidgetItem(tr("보통 예금"));
+                ui->userSearchTableWidget->setItem(row, 1, item);
+                item = new QTableWidgetItem(tr("X"));
+                ui->userSearchTableWidget->setItem(row, 3, item);
+            }
+            else if (dynamic_cast<Saving*>(account)) {
+                item = new QTableWidgetItem(tr("정기 예금"));
+                ui->userSearchTableWidget->setItem(row, 1, item);
+                item = new QTableWidgetItem(QString::number(account->get_duration()));
+                ui->userSearchTableWidget->setItem(row, 3, item);
+            }
+
+            item = new QTableWidgetItem(QString::number(account->get_balance()));
+            ui->userSearchTableWidget->setItem(row, 2, item);
+
+            item = new QTableWidgetItem(QString::fromStdString(make_time_string(account->get_start_date())));
+            ui->userSearchTableWidget->setItem(row, 4, item);
+            row++;
+        }
+    }
+}
+
+void ManagerWidget::refreshUserDeleteTable() {
+    ui->userIdTableWidget->reset();
+    int row = 0;
+    QTableWidgetItem* item;
+    for (const auto& datum : data) {
+        if (dynamic_cast<Manager*>(datum.second.second)) continue;
+
+        item = new QTableWidgetItem(QString::fromStdString(datum.first));
+        ui->userIdTableWidget->setItem(row++, 0, item);
+        connect(ui->userIdTableWidget, &QTableWidget::itemClicked, this, &ManagerWidget::refreshUserAccountsTable);
+    }
+}
+void ManagerWidget::refreshUserAccountsTable(QTableWidgetItem* item) {
+    ui->userAccountsTabelWidget->reset();
+    string userId = item->text().toStdString();
+    auto found = data.find(userId);
+    if (found == data.end()) return;
+    QTableWidgetItem* widgetItem;
+    int row = 0;
+    for (const auto& account : found->second.second->get_accounts()) {
+        if (dynamic_cast<Deposit*>(account)) {
+            widgetItem = new QTableWidgetItem(tr("보통 예금"));
+            ui->userAccountsTabelWidget->setItem(row, 0, widgetItem);
+            widgetItem = new QTableWidgetItem(tr("X"));
+            ui->userAccountsTabelWidget->setItem(row, 2, widgetItem);
+        }
+        else if (dynamic_cast<Saving*>(account)) {
+            widgetItem = new QTableWidgetItem(tr("정기 예금"));
+            ui->userAccountsTabelWidget->setItem(row, 0, widgetItem);
+            widgetItem = new QTableWidgetItem(QString::number(account->get_duration()));
+            ui->userAccountsTabelWidget->setItem(row, 2, widgetItem);
+        }
+
+        widgetItem = new QTableWidgetItem(QString::number(account->get_balance()));
+        ui->userAccountsTabelWidget->setItem(row, 1, widgetItem);
+        widgetItem = new QTableWidgetItem(QString::fromStdString(make_time_string(account->get_start_date())));
+        ui->userAccountsTabelWidget->setItem(row, 3, widgetItem);
+        row++;
+    }
 }
--- managerwidget.h
+++ managerwidget.h
@@ -1,7 +1,14 @@
 #ifndef MANAGERWIDGET_H
 #define MANAGERWIDGET_H
 
+#include <map>
+#include <string>
+using namespace std;
+
 #include <QWidget>
+
+class Person;
+class QTableWidgetItem;
 
 namespace Ui {
 class ManagerWidget;
@@ -14,9 +21,26 @@ class ManagerWidget : public QWidget
 public:
     explicit ManagerWidget(QWidget *parent = nullptr);
     ~ManagerWidget();
+    void setData(map<string, pair<string, Person*>> d);
+    void setPerson(Person* person);
+    void setId(string id);
 
 private:
     Ui::ManagerWidget *ui;
+
+    map<string, pair<string, Person*>> data; // id: <pw,person>
+    string id;
+    Person* person;
+
+    void refreshUserSearchTable(string searchId);
+    void refreshUserDeleteTable();
+
+signals:
+    void switchTomainScreen();
+    void withdrawManager(string id);
+
+public slots:
+    void refreshUserAccountsTable(QTableWidgetItem* item);
 };
 
 #endif // MANAGERWIDGET_H
