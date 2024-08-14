e62b1ba2047a7fd7a1988b5dcdba290f0670f227
박성민 <sm136599@gmail.com>
2024-08-14 20:03:28 +0900

    refactor: 매니저 화면 로직 변경
    -유저 삭제 탭 클릭 안됨
    -유저 조회 잔액 코마 추가
    -테이블 위젯 리셋 함수 변경

--- managerwidget.cpp
+++ managerwidget.cpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <QTableWidget>
 #include <QTableWidgetItem>
+#include <QLocale>
 
 ManagerWidget::ManagerWidget(QWidget *parent)
     : QWidget(parent)
@@ -44,9 +45,7 @@ ManagerWidget::ManagerWidget(QWidget *parent)
         refreshUserDeleteTable();
         refreshUserSearchTable(string(""));
         ui->userAccountsTabelWidget->setRowCount(0);
-        ui->userAccountsTabelWidget->clearSelection();
-        ui->userAccountsTabelWidget->reset();
-        ui->userAccountsTabelWidget->clear();
+        ui->userAccountsTabelWidget->clearContents();
 
     });
     refreshUserSearchTable("");
@@ -72,7 +71,7 @@ void ManagerWidget::setId(string id) {
 }
 
 void ManagerWidget::refreshUserSearchTable(string searchId) {
-    ui->userSearchTableWidget->reset();
+    ui->userSearchTableWidget->clearContents();
 
     int cnt = 0;
     for (const auto& d: data) {
@@ -95,6 +94,7 @@ void ManagerWidget::refreshUserSearchTable(string searchId) {
 
     int row = 0;
     QTableWidgetItem* item;
+    QLocale locale = QLocale::system();
     for (const auto& datum : data) {
         qDebug() << datum.first;
         if (dynamic_cast<Manager*>(datum.second.second)) continue;
@@ -108,25 +108,39 @@ void ManagerWidget::refreshUserSearchTable(string searchId) {
 
         for (const auto& account : userAccounts) {
             item = new QTableWidgetItem(QString::fromStdString(userId));
+            item->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+            item->setFlags(item->flags() & ~Qt::ItemIsEditable);
             ui->userSearchTableWidget->setItem(row, 0, item);
 
             if (dynamic_cast<Deposit*>(account)){
                 item = new QTableWidgetItem(tr("보통 예금"));
+                item->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+                item->setFlags(item->flags() & ~Qt::ItemIsEditable);
                 ui->userSearchTableWidget->setItem(row, 1, item);
                 item = new QTableWidgetItem(tr("X"));
+                item->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+                item->setFlags(item->flags() & ~Qt::ItemIsEditable);
                 ui->userSearchTableWidget->setItem(row, 3, item);
             }
             else if (dynamic_cast<Saving*>(account)) {
                 item = new QTableWidgetItem(tr("정기 예금"));
+                item->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+                item->setFlags(item->flags() & ~Qt::ItemIsEditable);
                 ui->userSearchTableWidget->setItem(row, 1, item);
                 item = new QTableWidgetItem(QString::number(account->get_duration()));
+                item->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+                item->setFlags(item->flags() & ~Qt::ItemIsEditable);
                 ui->userSearchTableWidget->setItem(row, 3, item);
             }
 
-            item = new QTableWidgetItem(QString::number(account->get_balance()));
+            item = new QTableWidgetItem(locale.toString(account->get_balance()));
+            item->setTextAlignment(Qt::AlignRight | Qt::AlignVCenter);
+            item->setFlags(item->flags() & ~Qt::ItemIsEditable);
             ui->userSearchTableWidget->setItem(row, 2, item);
 
             item = new QTableWidgetItem(QString::fromStdString(make_time_string(account->get_start_date())));
+            item->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+            item->setFlags(item->flags() & ~Qt::ItemIsEditable);
             ui->userSearchTableWidget->setItem(row, 4, item);
             row++;
         }
@@ -134,7 +148,7 @@ void ManagerWidget::refreshUserSearchTable(string searchId) {
 }
 
 void ManagerWidget::refreshUserDeleteTable() {
-    ui->userIdTableWidget->reset();
+    ui->userIdTableWidget->clearContents();
 
     int cnt = 0;
     for (const auto& d: data) {
@@ -145,6 +159,7 @@ void ManagerWidget::refreshUserDeleteTable() {
     }
 
     ui->userIdTableWidget->setRowCount(cnt);
+    ui->userIdTableWidget->verticalHeader()->setVisible(false);
 
     int row = 0;
     QTableWidgetItem* item;
@@ -153,39 +168,51 @@ void ManagerWidget::refreshUserDeleteTable() {
         if (dynamic_cast<Manager*>(datum.second.second)) continue;
 
         item = new QTableWidgetItem(QString::fromStdString(datum.first));
+        item->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+        item->setFlags(item->flags() & ~Qt::ItemIsEditable);
         ui->userIdTableWidget->setItem(row++, 0, item);
         connect(ui->userIdTableWidget, &QTableWidget::itemClicked, this, &ManagerWidget::refreshUserAccountsTable);
     }
-    ui->userAccountsTabelWidget->setRowCount(0);
-    ui->userAccountsTabelWidget->clearSelection();
-    ui->userAccountsTabelWidget->reset();
-    ui->userAccountsTabelWidget->clear();
+    ui->userAccountsTabelWidget->clearContents();
 }
 void ManagerWidget::refreshUserAccountsTable(QTableWidgetItem* item) {
-    ui->userAccountsTabelWidget->reset();
+    ui->userAccountsTabelWidget->clearContents();
     string userId = item->text().toStdString();
     auto found = data.find(userId);
     if (found == data.end()) return;
     QTableWidgetItem* widgetItem;
+    QLocale locale = QLocale::system();
     ui->userAccountsTabelWidget->setRowCount(found->second.second->get_accounts().size());
     int row = 0;
     for (const auto& account : found->second.second->get_accounts()) {
         if (dynamic_cast<Deposit*>(account)) {
             widgetItem = new QTableWidgetItem(tr("보통 예금"));
+            widgetItem->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+            widgetItem->setFlags(item->flags() & ~Qt::ItemIsEditable);
             ui->userAccountsTabelWidget->setItem(row, 0, widgetItem);
             widgetItem = new QTableWidgetItem(tr("X"));
+            widgetItem->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+            widgetItem->setFlags(item->flags() & ~Qt::ItemIsEditable);
             ui->userAccountsTabelWidget->setItem(row, 2, widgetItem);
         }
         else if (dynamic_cast<Saving*>(account)) {
             widgetItem = new QTableWidgetItem(tr("정기 예금"));
+            widgetItem->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+            widgetItem->setFlags(item->flags() & ~Qt::ItemIsEditable);
             ui->userAccountsTabelWidget->setItem(row, 0, widgetItem);
             widgetItem = new QTableWidgetItem(QString::number(account->get_duration()));
+            widgetItem->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+            widgetItem->setFlags(item->flags() & ~Qt::ItemIsEditable);
             ui->userAccountsTabelWidget->setItem(row, 2, widgetItem);
         }
 
-        widgetItem = new QTableWidgetItem(QString::number(account->get_balance()));
+        widgetItem = new QTableWidgetItem(locale.toString(account->get_balance()));
+        widgetItem->setTextAlignment(Qt::AlignRight | Qt::AlignVCenter);
+        widgetItem->setFlags(item->flags() & ~Qt::ItemIsEditable);
         ui->userAccountsTabelWidget->setItem(row, 1, widgetItem);
         widgetItem = new QTableWidgetItem(QString::fromStdString(make_time_string(account->get_start_date())));
+        widgetItem->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+        widgetItem->setFlags(item->flags() & ~Qt::ItemIsEditable);
         ui->userAccountsTabelWidget->setItem(row, 3, widgetItem);
         row++;
     }
